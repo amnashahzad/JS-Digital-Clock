ad0b58ccc6fa7c965665e5f008d8e00fb4e923b5
amnashahzad <121329321+amnashahzad@users.noreply.github.com>
2023-04-10 18:32:46 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+    let person = prompt("Please enter your name");
+    if (person) {
+      document.getElementById("demo").innerHTML =
+      "Hello "+ person+" Welcome to my Digital Clock";
+    }
+
